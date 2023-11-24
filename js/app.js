c835bc78272b2157bc6778bf652d13c8fafe5215
Lucas Capdevila <lcapdevila60@gmail.com>
2023-11-23 21:24:07 -0300

    se agrega funciones para botones mostrar generacion, mayor de edad y mostrar datos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//  Variables necesarias
 const btnGeneracion = document.querySelector('.btn-warning')
 const btnMayorDeEdad = document.querySelector('.btn-danger')
 const formularioDatos = document.querySelector('form')
@@ -17,14 +18,27 @@ formularioDatos.onsubmit = (e) => {
   listaUsuarios.push(usuario1)
   formularioDatos.reset()
   console.log(usuario1);
-  console.log(listaUsuarios);
 }
 
-const mostrarGeneracion = () => {
 
-}
 
-btnGeneracion.addEventListener('click', mostrarGeneracion)
+//  Funciones
+btnGeneracion.addEventListener('click', () => {
+  listaUsuarios[0].mostrarGeneracion()
+})
+
+btnMayorDeEdad.addEventListener('click', () => {
+  listaUsuarios[0].esMayorDeEdad()
+})
+
+btnMostrarDatos.addEventListener('click', () => {
+  listaUsuarios[0].mostrarDatos()
+})
+
+
+
+
+
 
 class Persona{
   constructor(nombre, edad, dni, sexo, peso, altura, anioNacimiento, domicilio){
